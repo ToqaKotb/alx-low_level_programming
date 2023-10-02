95cad3a613a62449b0c3ce64cc215a3b02ca6825
Toqakotb <htoqa13@yahoo.com>
2023-10-02 03:36:45 -0700

    k

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints strings
+ * @s: the printed string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
