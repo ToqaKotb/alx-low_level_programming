9224981fb549dd4e9e3fa0c8392652b6d784d56c
ToqaKotb <htoqa13@yahoo.com>
2023-10-22 07:56:20 -0700

    dd

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int factorial(int n)
+int _pow_recursion(int x, int y);
+int factorial(int n);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
